b27a1522c35dabd249ebf8fef5cc04ad5715a487
Kate Lint <katelint@Kates-MacBook-Pro.local>
2022-02-06 20:33:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aidlexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aidlexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var TAG = "android_aidl_example"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
