a2df7bbd21bf10df7d2163be18cc6679a7713d15
katelint <ktlint@gmail.com>
2022-02-07 15:22:15 +0200

    aidlpackage

--- app/src/main/java/com/example/aidlexample/MainActivity.kt
+++ app/src/main/java/com/example/aidlexample/MainActivity.kt
@@ -1,17 +1,53 @@
 package com.example.aidlexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aidlexample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private var TAG = "android_aidl_example"
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addNumbers( Integer.parseInt(binding.editFirstNumber.toString()), Integer.parseInt(binding.editSecondtNumber.toString()))
+
+        bingToAidlService();
+
+    }
+
+    fun bingToAidlService(){
+
+       var aidlServiceIntent = Intent("connection_to_aild_service")
+
+        bindService(aidlServiceIntent,mConnection, BIND_AUTO_CREATE)
+    }
+
+    private val mConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(
+            className: ComponentName,
+            service: IBinder
+        ) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            SepratePackage.IAidlInterface.Stub(service)
+        }
+
+    }
 
 
+
+    fun addNumbers(firstNumber: Int, seconfNumber:Int): Int {
+        return firstNumber + seconfNumber
 
     }
 }
